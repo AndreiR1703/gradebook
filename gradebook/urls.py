2fdceeecc18d51027aacb335a4769539ca7145e3
AndreiR1703 <andr3y_rat@yahoo.com>
2023-10-28 14:31:25 +0300

    functie de signup noua

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     path("", views.show_first_page, name="home"),
     path("", include("django.contrib.auth.urls")),
     # path("signup/", views.SignUpView.as_view() ,name="signup"),
-    path("signup/", views.SignUpView.as_view() ,name="signup"),
+    path("signup/", views.SignUpView.as_view(),name="signup"),
     path("showgrades/", views.show_grades, name="show-grades"),
     path("addgrade/", views.GradeCreateView.as_view(), name="add-grade"),
     path("updategrade/<int:pk>/", views.GradeUpdateView.as_view(), name="update-grade"),
