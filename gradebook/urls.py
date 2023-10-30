5cfd2cf48dfdfecb6b91dc0e3452f75f20d816ea
AndreiR1703 <andr3y_rat@yahoo.com>
2023-10-30 08:41:08 +0200

    functie de login + logout functionale

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from gradebook import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path("", views.show_first_page, name="home"),
     # path("", include("django.contrib.auth.urls")),
     # path("signup/", views.SignUpView.as_view() ,name="signup"),
     path("login/", views.LogInView.as_view(), name="login"),
+    path("logout/",auth_views.LogoutView.as_view(),name="logout"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("showgrades/", views.show_grades, name="show-grades"),
     path("addgrade/", views.GradeCreateView.as_view(), name="add-grade"),
